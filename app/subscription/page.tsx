85926f91cf695fa675874d74771c2ed69a3ac3ed
GustavoYM01 <gumatimoto@hotmail.com>
2024-11-11 20:47:37 -0300

    feat: ensure transactions and subscriptions pages are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,11 @@
-const SubscriptionPage = () => {
-  return (  );
-}
- 
+import { auth } from "@clerk/nextjs/server";
+import Navbar from "../_components/navbar";
+import { redirect } from "next/navigation";
+
+const SubscriptionPage = async () => {
+  const { userId } = await auth();
+  if (!userId) redirect("/login");
+  return <Navbar />;
+};
+
 export default SubscriptionPage;
